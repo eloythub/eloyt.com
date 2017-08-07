0eee24c4bfbbd4ab421cc83d663fcaf59dd5539d
Mahan <mahan@gomeeki.com>
2017-08-07 11:00:50 +0700

    update + healthcheck

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app     = express()
 
 app.use(express.static('app'))
 
+app.get('/healthz', (req, res) => {
+  res.send('OK');
+});
+
 app.listen(process.env['EXPOSE_PORT'] || 80, () => {
   console.log(`eloyt.com is exposed on port: ${process.env['EXPOSE_PORT'] || 80}`)
 })
