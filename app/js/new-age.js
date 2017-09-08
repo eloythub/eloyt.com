3b4e4a1a6ae7ef879416f135a8ce074f38dbe0ad
Mahan <mahan@gomeeki.com>
2017-09-08 19:31:40 +0700

    update view

--- app/js/new-age.js
+++ app/js/new-age.js
@@ -1,31 +1,37 @@
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+  "use strict" // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $('a.page-scroll').bind('click', function (event) {
+    try {
+      var $anchor = $(this)
+      $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+      }, 1250, 'easeInOutExpo')
+      event.preventDefault()
+    } catch (err) {
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
-    });
+    }
+  })
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 100
+  })
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 50
-        }
-    })
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click()
+  })
 
-})(jQuery); // End of use strict
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 50
+    }
+  })
+
+  particlesJS.load('eloyt-intro-particles', 'data/particlesjs-config.json')
+  particlesJS.load('eloyt-download-particles', 'data/particlesjs-config.json')
+})(jQuery) // End of use strict
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp        = require('gulp');
-var less        = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var header      = require('gulp-header');
-var cleanCSS    = require('gulp-clean-css');
-var rename      = require("gulp-rename");
-var uglify      = require('gulp-uglify');
-var pkg         = require('./package.json');
+var gulp        = require('gulp')
+var less        = require('gulp-less')
+var browserSync = require('browser-sync').create()
+var header      = require('gulp-header')
+var cleanCSS    = require('gulp-clean-css')
+var rename      = require("gulp-rename")
+var uglify      = require('gulp-uglify')
+var concat      = require('gulp-concat')
+var pkg         = require('./package.json')
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -14,82 +15,74 @@ var banner = ['/*!\n',
   ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
   ' */\n',
   ''
-].join('');
+].join('')
 
 // Compile LESS files from /less into /css
 gulp.task('less', function () {
   return gulp.src('app/less/new-age.less')
-             .pipe(less())
-             .pipe(header(banner, {pkg: pkg}))
-             .pipe(gulp.dest('app/css'))
-             .pipe(browserSync.reload({
-               stream: true
-             }))
-});
+    .pipe(less())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest('app/css'))
+})
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function () {
   return gulp.src('app/css/new-age.css')
-             .pipe(cleanCSS({compatibility: 'ie8'}))
-             .pipe(rename({suffix: '.min'}))
-             .pipe(gulp.dest('app/css'))
-             .pipe(browserSync.reload({
-               stream: true
-             }))
-});
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+})
 
 // Minify JS
 gulp.task('minify-js', function () {
-  return gulp.src('app/js/new-age.js')
-             .pipe(uglify())
-             .pipe(header(banner, {pkg: pkg}))
-             .pipe(rename({suffix: '.min'}))
-             .pipe(gulp.dest('app/js'))
-             .pipe(browserSync.reload({
-               stream: true
-             }))
-});
+  return gulp.src([
+      'app/vendor/jquery/jquery.js',
+      'app/vendor/bootstrap/js/bootstrap.js',
+      'app/vendor/particles.js/particles.js',
+      'app/js/new-age.js'
+    ])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/js'))
+})
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function () {
   gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-      .pipe(gulp.dest('app/vendor/bootstrap'))
+    .pipe(gulp.dest('app/vendor/bootstrap'))
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-      .pipe(gulp.dest('app/vendor/jquery'))
+    .pipe(gulp.dest('app/vendor/jquery'))
 
   gulp.src(['node_modules/simple-line-icons/*/*'])
-      .pipe(gulp.dest('app/vendor/simple-line-icons'))
+    .pipe(gulp.dest('app/vendor/simple-line-icons'))
+
+  gulp.src(['node_modules/particles.js/*'])
+    .pipe(gulp.dest('app/vendor/particles.js'))
 
   gulp.src([
-        'node_modules/font-awesome/**',
-        '!node_modules/font-awesome/**/*.map',
-        '!node_modules/font-awesome/.npmignore',
-        '!node_modules/font-awesome/*.txt',
-        '!node_modules/font-awesome/*.md',
-        '!node_modules/font-awesome/*.json'
-      ])
-      .pipe(gulp.dest('app/vendor/font-awesome'))
+      'node_modules/font-awesome/**',
+      '!node_modules/font-awesome/**/*.map',
+      '!node_modules/font-awesome/.npmignore',
+      '!node_modules/font-awesome/*.txt',
+      '!node_modules/font-awesome/*.md',
+      '!node_modules/font-awesome/*.json'
+    ])
+    .pipe(gulp.dest('app/vendor/font-awesome'))
 })
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy'])
 
-// Configure the browserSync task
-gulp.task('browserSync', function () {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function () {
-  gulp.watch('app/less/*.less', ['less']);
-  gulp.watch('app/css/*.css', ['minify-css']);
-  gulp.watch('app/js/*.js', ['minify-js']);
+gulp.task('dev', ['less', 'minify-css', 'minify-js'], function () {
+  gulp.watch('app/less/*.less', ['less'])
+  gulp.watch('app/css/*.css', ['minify-css'])
+  gulp.watch('app/js/*.js', ['minify-js'])
   // Reloads the browser whenever HTML or JS files change
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-});
+  gulp.watch('app/*.html', browserSync.reload)
+  gulp.watch('app/js/**/*.js', browserSync.reload)
+})
