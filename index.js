31d8ab17137d8c91fce60170a6450e02a73a67d5
Mahan <mahan@gomeeki.com>
2017-10-21 18:19:36 +0700

    terms and faq

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const path    = require('path')
 const express = require('express')
 const app     = express()
 
 app.use(express.static('app'))
 
 app.get('/healthz', (req, res) => {
-  res.send('OK');
-});
+  res.send('OK')
+})
+
+app.use('/terms', express.static(path.join(__dirname, 'app', 'terms.html')))
+app.use('/faq', express.static(path.join(__dirname, 'app', 'faq.html')))
 
 app.listen(process.env['EXPOSE_PORT'] || 80, () => {
   console.log(`eloyt.com is exposed on port: ${process.env['EXPOSE_PORT'] || 80}`)
